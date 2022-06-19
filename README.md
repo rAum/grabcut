d8ecf97f206ca182ba5aedfe6658254c816e61ce
Piotr Gródek <grodekpiotr+gh@gmail.com>
2022-06-19 10:11:21 +0200

    Init project

---
+++ README.md
@@ -0,0 +1,3 @@
+# Grabcut
+
+This is WIP rewrite of my uni project I did in 2010 for image processing class.
---
+++ apps/run_grabcut.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+
+int main(int , char**) {
+    std::cout << "This is test for running grabcut " << std::endl;
+    return 0;
+}
