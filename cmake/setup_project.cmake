7d8a3fb520ee4bc109adff69893d9c3e6bf2f855
Piotr Gródek <grodekpiotr+gh@gmail.com>
2022-06-20 12:43:34 +0200

    Make C++ standard version target-based instead of global cmake setting

--- cmake/setup_project.cmake
+++ cmake/setup_project.cmake
@@ -1,7 +1,3 @@
-set(CMAKE_CXX_STANDARD 20)
-set(CMAKE_CXX_STANDARD_REQUIRED ON)
-set(CMAKE_CXX_EXTENSIONS OFF)
-
 if(CMAKE_GENERATOR MATCHES "Makefiles|Ninja" AND CMAKE_BUILD_TYPE STREQUAL "")
     message(STATUS "No build type specified - set CMAKE_BUILD_TYPE=Release.")
     set(CMAKE_BUILD_TYPE Release CACHE STRING "Build type" FORCE)
@@ -17,4 +13,11 @@ endif()
 
 function(enable_project_settings Target)
     target_compile_options(${Target} PRIVATE "-Werror" "-Wall" "-Wpedantic" "-Wextra" "-Wno-unused-parameter")
+    set_target_properties(${Target} PROPERTIES
+            CXX_STANDARD_REQUIRED ON
+            CXX_EXTENSIONS OFF
+            CXX_STANDARD 20)
+    set_target_properties(${Target} PROPERTIES
+            CXX_VISIBILITY_PRESET hidden
+            VISIBILITY_INLINES_HIDDEN 1)
 endfunction()
