3874e3d6809aaac129fdd679724915d1450348c3
Piotr Gródek <grodekpiotr+gh@gmail.com>
2022-06-20 12:13:37 +0200

    Add very simple tool to select ROI in images

--- apps/run_grabcut.cpp
+++ apps/run_grabcut.cpp
@@ -1,6 +1,38 @@
+/// This is a simple GUI for interactive grabcut tests
 #include <iostream>
+#include <string>
+#include <filesystem>
 
-int main(int , char**) {
-    std::cout << "This is test for running grabcut " << std::endl;
+#include <opencv2/highgui.hpp>
+#include <opencv2/core.hpp>
+
+int main(int argc, char** argv) {
+    if (argc <= 1) {
+        std::cerr << "Pass image file" << std::endl;
+        return 1;
+    }
+    cv::Mat input_image;
+    try {
+        std::filesystem::path image_path(argv[1]);
+        input_image = cv::imread(image_path.c_str());
+        std::cout << "const std::string file = " << std::quoted(argv[1]) << "\n";
+    } catch (...) {
+        std::cerr << "Failed to load " << std::quoted(argv[1]) << std::endl;
+        return 3;
+    }
+
+    std::string window_name = "Grabcut Interactive Demo";
+    cv::namedWindow(window_name);
+
+    cv::Rect selection;
+    do {
+        selection = cv::selectROI(window_name, input_image);
+    } while (selection.area() < 10);
+    std::cout << "cv::Rect selection = { "
+        << selection.x << ", " << selection.y << ", "
+        << selection.width << ", " << selection.height << "};\n";
+    auto result_image = cv::Mat::zeros(input_image.size(), CV_8UC4);
+    cv::waitKey();
+    cv::destroyAllWindows();
     return 0;
 }
