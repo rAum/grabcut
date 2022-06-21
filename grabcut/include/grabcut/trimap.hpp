b64e762c5bde59404dccfc478a050f1a696b92ff
Piotr Gródek <grodekpiotr+gh@gmail.com>
2022-06-21 15:51:45 +0200

    Add enum for tri colorization and fix cmake test function

---
+++ grabcut/include/grabcut/trimap.hpp
@@ -0,0 +1,15 @@
+#pragma once
+#ifndef GRABCUT_TRIMAP_HPP
+#define GRABCUT_TRIMAP_HPP
+
+namespace grabcut {
+
+enum class Trimap {
+    Background,
+    Foreground,
+    Unknown,
+};
+
+}  // namespace grabcut
+
+#endif //GRABCUT_TRIMAP_HPP
