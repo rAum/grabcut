a51bcc212858cf5b743185ff9b3c0bfe9f1456dd
Piotr Gródek <grodekpiotr+gh@gmail.com>
2022-07-06 08:18:42 +0200

    Make app more usable for quick evaluation, remove OpenCV dependency on grabcut and keep it to app only

--- apps/run_grabcut.cpp
+++ apps/run_grabcut.cpp
@@ -39,37 +39,36 @@ int main(int argc, char** argv) {
     std::string window_name = "Grabcut Interactive Demo";
     cv::namedWindow(window_name);
 
-//    cv::Rect selection;
-//    do {
-//        selection = cv::selectROI(window_name, input_image);
-//    } while (selection.area() < 10);
-//    std::cout << "cv::Rect selection = { "
-//        << selection.x << ", " << selection.y << ", "
-//        << selection.width << ", " << selection.height << "};\n";
-//    auto result_image = cv::Mat::zeros(input_image.size(), CV_8UC4);
-
-    cv::Rect selection = { 68, 23, 539, 510};
-
-    cv::Rect rect = selection;
+    cv::Rect selection;
+    // for quick eval
+    if (std::string("../../tests/data/flower1.jpg") == argv[1]) {
+        selection = { 68, 23, 539, 510};
+    } else {
+        do {
+            selection = cv::selectROI(window_name, input_image);
+        } while (selection.area() < 10);
+        std::cout << "cv::Rect selection = { "
+                  << selection.x << ", " << selection.y << ", "
+                  << selection.width << ", " << selection.height << "};\n";
+        auto result_image = cv::Mat::zeros(input_image.size(), CV_8UC4);
+    }
     cv::TickMeter timer;
 
-    constexpr int steps = 2;
+    constexpr int steps = 1;
 
     timer.start();
     cv::Mat src = input_image;
     cv::Mat mask = cv::Mat::zeros(src.rows, src.cols, CV_8UC1);
     cv::Mat bgModel = cv::Mat::zeros(1, 65, CV_64FC1);
     cv::Mat fgModel = cv::Mat::zeros(1, 65, CV_64FC1);
-    cv::grabCut(src, mask, rect, bgModel, fgModel, steps, cv::GC_INIT_WITH_RECT);
+    cv::grabCut(src, mask, selection, bgModel, fgModel, steps, cv::GC_INIT_WITH_RECT);
     timer.stop();
     std::cout << "OpenCV time: " << timer.getTimeSec() << std::endl;
 
     cv::Mat mask2 = (mask == 1) + (mask == 3);  // 0 = cv::GC_BGD, 1 = cv::GC_FGD, 2 = cv::PR_BGD, 3 = cv::GC_PR_FGD
     cv::Mat dest;
     src.copyTo(dest, mask2);
-
-    // 6. show result
-    cv::imshow("dest", dest);
+    cv::imshow("baseline_opencv", dest);
 
     timer.start();
     auto maskm = get_rect_mask(src.cols, src.rows, selection);
@@ -77,13 +76,13 @@ int main(int argc, char** argv) {
     myimpl.init(src.data, maskm.get(), src.cols, src.rows);
     myimpl.run(steps);
     timer.stop();
-    std::cout << "my time: " << timer.getTimeSec() << std::endl;
+    std::cout << "Time: " << timer.getTimeSec() << std::endl;
     auto res = myimpl.get_mask();
-    //for (auto& r : res) r *= 255;
+
     cv::Mat cv_res{src.rows, src.cols, CV_8UC1, res.data() };
     cv::Mat dest2;
     src.copyTo(dest2, cv_res);
-    cv::imshow("myimpl", dest2);
+    cv::imshow("this_implementation", dest2);
 
     cv::waitKey();
     return 0;
