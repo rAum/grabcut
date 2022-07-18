277bb6c3d39aecc5e080da1ab1c8f19fb1189527
Piotr Gródek <grodekpiotr+gh@gmail.com>
2022-07-18 15:52:37 +0200

    Add simple OpenCV API adapter

---
+++ grabcut/include/grabcut/opencv/grabcut.h
@@ -0,0 +1,45 @@
+#pragma once
+#ifndef GRABCUT_OPENCV_GRABCUT_H
+#define GRABCUT_OPENCV_GRABCUT_H
+
+#include <opencv2/core.hpp>
+#include <grabcut/grabcut.h>
+
+namespace grabcut {
+
+/***
+ * This is an adapter for OpenCV API compatibility, to simplify basic call.
+ * For better control and performance, just use API directly.
+ *
+ * Example:
+ * >>> cv::Rect selection{ 10, 10, 50, 50 }; // ROI rectangle
+ * >>> cv::Mat mask = cv::Mat::zeros(image.rows, image.cols, CV_8UC1);
+ * >>> cv::rectangle(mask, selection, cv::Scalar(1), -1);
+ * >>> grabcut::run_grabcut(image, mask); // segment image given mask
+ * >>> image.copyTo(destination, mask); // copy only pixels from foreground
+ *
+ * @param image BGR or RGB 8-bit image
+ * @param mask input-output defined
+ * @param iterations maximum number of iterations to be performed.
+ */
+void run_grabcut(cv::InputArray image, cv::InputOutputArray mask, int iterations = 3) {
+    CV_Assert(image.isContinuous());
+    CV_Assert(image.type() == CV_8UC3);
+    CV_Assert(mask.type() == CV_8UC1);
+    CV_Assert(mask.isContinuous());
+
+    auto m = mask.getMat();
+    auto src = image.getMat();
+    CV_Assert(src.size == m.size);
+
+    Grabcut grabcut;
+    grabcut.init(src.data, m.data, src.cols, src.rows);
+    grabcut.run(iterations);
+    auto res = grabcut.get_mask();
+    cv::Mat cv_res{src.rows, src.cols, CV_8UC1, res.data() };
+    cv_res.copyTo(mask);
+}
+
+} // namespace grabcut
+
+#endif // GRABCUT_OPENCV_GRABCUT_H
